545fc7f2250a3b9d1f5594a27abc3ccf57efa275
Neilhm <neilhamzi12@gmail.com>
2022-11-30 13:10:16 +0200

    update jdk
    aerfdsa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module application.knightsmove {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
 
     opens controller to javafx.fxml;
     exports controller;
