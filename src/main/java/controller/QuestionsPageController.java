9eb9051205e3b79ef0ef936e55777c41fccfd717
rani-maklada <rani_m.14@hotmail.com>
2022-12-16 18:15:37 +0200

    add-listViewAnswers

--- src/main/java/controller/QuestionsPageController.java
+++ src/main/java/controller/QuestionsPageController.java
@@ -20,6 +20,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
@@ -34,13 +35,23 @@ public class QuestionsPageController {
 	@FXML private Parent root;
 	@FXML private Scene scene;
 	@FXML private Stage stage;
-	@FXML  private ListView<Label> listView;
+	@FXML  private ListView<Label> listViewQuestions;
+	@FXML private ListView<Label> listViewAnswers;
+
+	@FXML private TextField tfCorrectAnswer;
+
+	@FXML private TextField tfLevel;
+
+	@FXML private TextField tfTeam;
 	@FXML
 	void initialize() throws IOException {
 		resetViewList();
+		listViewQuestions.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			getAnswers();
+		});
 	}
 	void resetViewList(){
-		listView.getItems().clear();
+		listViewQuestions.getItems().clear();
 		for (Question q : SysData.getInstance().getQuestions()) {
 			Button myButton = new Button();
 			myButton.setText("Click me");
@@ -58,16 +69,36 @@ public class QuestionsPageController {
 				lbItem.setTextFill(Color.WHITE);
 			}
 
-			listView.getItems().add(lbItem);
+			listViewQuestions.getItems().add(lbItem);
 		}
-		listView.refresh();
+		listViewQuestions.refresh();
 	}
 	@FXML
 	void DeleteButton(ActionEvent event) {
-		System.out.println(listView.getSelectionModel().getSelectedItem());
-		SysData.getInstance().removeQuestion(listView.getSelectionModel().getSelectedItem().getText());
+		System.out.println(listViewQuestions.getSelectionModel().getSelectedItem());
+		SysData.getInstance().removeQuestion(listViewQuestions.getSelectionModel().getSelectedItem().getText());
 		resetViewList();
 
+	}
+
+	void getAnswers(){
+		listViewAnswers.getItems().clear();
+
+		int index = SysData.getInstance().getQuestions().indexOf(new Question(listViewQuestions.getSelectionModel().getSelectedItem().getText()));
+		Question q = SysData.getInstance().getQuestions().get(index);
+		for (String answer : q.getAnswers().values()) {
+			Label lbItem = new Label();
+			lbItem.setStyle("-fx-text-alignment: center;");
+			lbItem.setText(answer);
+			listViewAnswers.getItems().add(lbItem);
+		}
+		listViewAnswers.refresh();
+		fillTextFields(q);
+	}
+	void fillTextFields(Question q){
+		tfCorrectAnswer.setText(String.valueOf(q.getAnswers().get(q.getCorrect_ans())));
+		tfLevel.setText(String.valueOf(q.getLevel()));
+		tfTeam.setText(q.getTeam());
 	}
 
     @FXML
