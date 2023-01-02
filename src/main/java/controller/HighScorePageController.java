e5badad9aad99ed8b143b0ddfc69f9d10812bd0e
rani-maklada <rani_m.14@hotmail.com>
2023-01-02 13:19:44 +0200

    GameHistory-tableView

--- src/main/java/controller/HighScorePageController.java
+++ src/main/java/controller/HighScorePageController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,19 +9,39 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.GameHistory;
+import model.SysData;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class HighScorePageController {
     @FXML private Parent root;
     @FXML private Scene scene;
     @FXML private Stage stage;
-    @FXML private TableView<String> tableView;
+    @FXML private TableView<GameHistory> tableView;
+    @FXML
+    private TableColumn<GameHistory, String> colPlayerName;
+
+    @FXML
+    private TableColumn<GameHistory, Integer> colScore;
+
+    @FXML
+    private TableColumn<GameHistory, LocalDate> colDate;
     @FXML
     void initialize() throws IOException {
-        System.out.println(tableView);
+        // Set the cell value factories
+        colPlayerName.setCellValueFactory(new PropertyValueFactory<>("playerName"));
+        colScore.setCellValueFactory(new PropertyValueFactory<>("score"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        ObservableList<GameHistory> data = FXCollections.observableArrayList(SysData.getInstance().getHistory());
+        tableView.setItems(data);
+        System.out.println(SysData.getInstance().getHistory());
     }
     @FXML
     void BackButton(ActionEvent event) throws IOException {
