7c8da4c9515d1d5ab2aa5913234a1fc14b659bf0
Jeries882 <jeries.shomaly.98@hotmail.com>
2023-01-03 16:43:39 +0200

    GameOver

--- src/main/java/model/Knight.java
+++ src/main/java/model/Knight.java
@@ -32,13 +32,30 @@ public class Knight extends Piece{
     }
     private List<int[]> getKnightMoves(int x, int y) {
         List<int[]> moves = new ArrayList<>();
-        int[] dx, dy;
+        int[] dx, dy, dx2, dy2;
         if(GamePageController.myStage == 1){
             dx = new int[] {-1, 1, 2, -2, -2, 2, -1, 1};
             dy = new int[] {-2, -2, -1, -1, 1, 1, 2, 2};
+
         }else{
-            dx = new int[] {-2, -2, -1, 1, 2, 2, 1, -1};
-            dy = new int[] {-1, 1, 2, 2, 1, -1, -2, -2};
+            System.out.println("Here");
+            int[] dx3 = new int[] {-3, -3, -1, 1, 3, 3, 1, -1};
+            int []dy3= new int[] {-1, 1, 3, 3, 1, -1, -3, -3};
+             dx = new int[] {-3, -3, -2, 2, 3, 3, 2, -2};
+             dy = new int[] {-2, 2, 3, 3, 2, -2, -3, -3};
+             dx2 = new int[] {-2, -2, -1, 1, 2, 2, 1, -1};
+             dy2 = new int[] {-1, 1, 2, 2, 1, -1, -2, -2};
+
+            for (int i = 0; i < 8; i++) {
+                int newX = (x + dx2[i] % 8 + 8) % 8;
+                int newY = (y + dy2[i] % 8 + 8) % 8;
+                moves.add(new int[]{newX, newY});
+            }
+            for (int i = 0; i < 8; i++) {
+                int newX = (x + dx3[i] % 8 + 8) % 8;
+                int newY = (y + dy3[i] % 8 + 8) % 8;
+                moves.add(new int[]{newX, newY});
+            }
         }
         for (int i = 0; i < 8; i++) {
             int newX = (x + dx[i] % 8 + 8) % 8;
